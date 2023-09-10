b78e809c00ed964eb6ec6555e4c4d2d05e4448d7
mtedf <tedfor485@morrisville.edu>
2023-09-10 12:54:09 -0400

    Time.deltaTime

--- Delivery driver/Assets/Driver.cs
+++ Delivery driver/Assets/Driver.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    [SerializeField]float fltsteerSpeed = 1f;
-    [SerializeField]float fltmoveSpeed = 0.01f;
+    [SerializeField]float fltsteerSpeed = 300f;
+    [SerializeField]float fltmoveSpeed = 20f;
     
     // Start is called before the first frame update
     void Start()
@@ -17,8 +17,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float fltsteerAmount = UnityEngine.Input.GetAxis("Horizontal") * fltsteerSpeed;
-        float fltmoveAmount = UnityEngine.Input.GetAxis("Vertical") * fltmoveSpeed;
+        float fltsteerAmount = UnityEngine.Input.GetAxis("Horizontal") * fltsteerSpeed * Time.deltaTime;
+        float fltmoveAmount = UnityEngine.Input.GetAxis("Vertical") * fltmoveSpeed * Time.deltaTime;
         transform.Translate(0, fltmoveAmount, 0);
         transform.Rotate(0, 0, -fltsteerAmount);
     }
