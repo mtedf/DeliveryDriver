8b8e39b7522916af2ea6a5eeac059a8f043f292c
mtedf <tedfor485@morrisville.edu>
2023-09-13 13:15:17 -0400

    How to use bools

--- Delivery driver/Assets/Delivery.cs
+++ Delivery driver/Assets/Delivery.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool boolHasPackage = false;
+    
     private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!");
@@ -13,10 +15,12 @@ public class Delivery : MonoBehaviour
         if (other.tag == "Package")
         {
             Debug.Log("Package picked up");
+            boolHasPackage = true;
         }
-        if (other.tag == "Customer")
+        if (other.tag == "Customer" && boolHasPackage)
         {
             Debug.Log("Package delivered");
+            boolHasPackage = false;
         }
 
     }
